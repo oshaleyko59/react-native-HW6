7b7f483ad230f21934c6f12c008ca2e198307155
oshaleyko59 <oshaleyko@yahoo.com>
2023-08-04 11:13:56 -0500

    some fixes

--- src/store/auth/authOperations.js
+++ src/store/auth/authOperations.js
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import {
 	createUserWithEmailAndPassword,
 	signInWithEmailAndPassword,
@@ -6,7 +7,6 @@ import {
 	signOut,
 } from "firebase/auth";
 import { auth } from "../../firebase/config";
-
 
 export const authStateChanged = async (onChange = () => {}) => {
 	onAuthStateChanged((user) => {
@@ -18,9 +18,9 @@ export const authStateChanged = async (onChange = () => {}) => {
  */
 const fetchCurrentUser = createAsyncThunk(
 	"auth/refresh",
-  async (_, thunkAPI) => {
-    return thunkAPI.rejectWithValue(msg); // FIXME:
-/* 		const state = thunkAPI.getState();
+	async (_, thunkAPI) => {
+		return thunkAPI.rejectWithValue(msg); // FIXME:
+		/* 		const state = thunkAPI.getState();
     const persistedToken = state.auth.token;
 
     token.set(persistedToken);
@@ -43,7 +43,11 @@ const fetchCurrentUser = createAsyncThunk(
 );
 
 //register
-export const authRegister = async ({ email, password }) => {
+//export const authRegister = async ({ email, password }) => {
+  const register = createAsyncThunk(
+	"auth/register",
+  async (userData, thunkAPI) => {
+    const { email, password } = userData;
 	try {
 		const user = await auth.createUserWithEmailAndPassword(
 			auth,
@@ -52,31 +56,48 @@ export const authRegister = async ({ email, password }) => {
 		);
 		console.log("authRegister>>user", user);
 	} catch (e) {
-    console.error("authRegister>>error", e,  e.message);
-    throw e;
+		console.error("authRegister>>error", e, e.message);
+		const msg = transformErrorMsg(e.response);
+		return thunkAPI.rejectWithValue(msg);
 	}
-};
+});
 
 /* //TODO: або більш короткий запис цієї функції
 const registerDB = ({ email, password }) =>
         createUserWithEmailAndPassword(auth, email, password); */
 
 //login
-const authLogin = async ({ email, password }) => {
+//const authLogin = async ({ email, password }) => {
+  const login = createAsyncThunk("auth/login", async (credentials, thunkAPI) => {
+  const { email, password } = credentials;
+  console.debug("auth/login>>", email, password);
 	try {
 		const credentials = await signInWithEmailAndPassword(auth, email, password);
-    console.log("authLogin>>credentials", credentials);
-    return credentials.user;
+		console.log("authLogin>>credentials", credentials);
+		return credentials.user;
 	} catch (error) {
-		throw error;
+		console.log("auth/login>>error", error.response); // error,
+		const msg = transformErrorMsg(error.response);
+		return thunkAPI.rejectWithValue(msg);
 	}
-};
+  });
 
 /**
  * logout
  */
 //TODO: ???
-export const authLogout = async (dispatch, getState) => {};
+const logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+	try {
+    const response = await signOut(auth);
+    console.log("logout>>credentials", response);
+		token.remove();
+		//	thunkAPI.dispatch(removeContacts());
+		//	thunkAPI.dispatch(setFilter(""));
+	} catch (error) {
+		const msg = transformErrorMsg(error.response);
+		return thunkAPI.rejectWithValue(msg);
+	}
+});
 
 const updateUserProfile = async (update) => {
 	const user = auth.currentUser;
